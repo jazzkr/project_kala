127eb131e1033b550f116620d20d3577009afe8b
Krisztian Kurucz <kkurucz@kkurucz-rzlvdt.local>
2020-02-05 20:19:52 -0500

    Add WiP pixel planner

---
+++ sw/kala_pixel_planner/main.py
@@ -0,0 +1,61 @@
+import sys
+import random
+from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import QFile, Slot
+from PySide2 import QtGui, QtCore, QtWidgets, Qt
+from pixel import PixelScene
+
+@Slot()
+def handle_button_press():
+    print("Button clicked, Hello!")
+
+class MouseEventFilter(QtCore.QObject):
+    
+    def eventFilter(self, obj, event):
+                     
+        if event.type() is event.MouseButtonPress:
+            print("Ate mouse press", event.pos())
+            event.accept()
+            return True
+        else:
+            # standard event processing
+            return QtCore.QObject.eventFilter(self, obj, event)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    ui_file = QFile("test_app.ui")
+    ui_file.open(QFile.ReadOnly)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    # Load pixels as QGraphicsScene objects
+    for i in range(1, 257):
+        try:
+            x = int((i-1)/16)
+            y = (i-1) % 16
+
+            px = window.findChild(QtWidgets.QGraphicsView, "graphicsView_" + str(i))
+            px_scene = PixelScene(x, y, QtGui.QColor(255, 255, 255, 255))
+            px_scene.setParent(window)
+            px.setScene(px_scene)
+
+            #pixels[x][y] = px_scene
+            print("Setting x, y: ", x, y)
+        except Exception as e:
+            print(f"Failed to set pixel at {i}")
+            print(e)
+
+    # Set up buttons
+    play_button = window.findChild(QtWidgets.QPushButton, "play_button")
+    play_button.clicked.connect(handle_button_press)
+
+    window.setWindowTitle("Kala Pixel Planner")
+    #mouse_filter = MouseEventFilter()
+    #window.installEventFilter(mouse_filter)
+    window.show()
+
+    sys.exit(app.exec_())
---
+++ sw/kala_pixel_planner/pixel.py
@@ -0,0 +1,33 @@
+from PySide2.QtWidgets import QGraphicsScene
+from PySide2 import QtGui, QtWidgets
+import random
+
+class PixelScene(QGraphicsScene):
+
+    def __init__(self, x, y, color):
+        QGraphicsScene.__init__(self)
+        self.x = x
+        self.y = y
+        self.color = color
+        self.set_color(self.color)
+
+    def __str__(self):
+        c = self.backgroundBrush().color()
+        return f"Pixel ({self.x}, {self.y}) - R:{c.red()} G:{c.green()} B:{c.blue()}"
+
+    def set_color(self, color):
+        self.color = color
+        self.setBackgroundBrush(self.color)
+
+    def mousePressEvent(self, event):
+        print(self)
+        c = self.backgroundBrush().color()
+        
+        r_field = self.parent().findChild(QtWidgets.QLineEdit, "red_field")
+        r_field.setText(str(c.red()))
+        g_field = self.parent().findChild(QtWidgets.QLineEdit, "green_field")
+        g_field.setText(str(c.green()))
+        b_field = self.parent().findChild(QtWidgets.QLineEdit, "blue_field")
+        b_field.setText(str(c.blue()))
+
+        QGraphicsScene.mousePressEvent(self, event)
